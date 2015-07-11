bb2697ea4ae503981e9daa3c67c85e3c10d34eb3
Joseph Turk <joseph.r.turk@gmail.com>
2015-07-11 10:48:03 -0400

    Add page title tests and set root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root 'static_pages#home'
   get 'static_pages/about'
 
   # The priority is based upon order of creation: first created -> highest priority.
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
-  root 'application#hello'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,13 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | O'Brien Photography"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | O'Brien Photography"
   end
 
 end
