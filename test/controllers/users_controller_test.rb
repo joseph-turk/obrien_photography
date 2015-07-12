7f4b3ded4b689f091945cee11e67bd0ac587b052
Joseph Turk <joseph.r.turk@gmail.com>
2015-07-12 10:54:04 -0400

    Add test for signup page title

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  def setup
+    @base_title = "O'Brien Photography"
+  end
+  
   test "should get new" do
     get :new
     assert_response :success
+    assert_select "title", "Sign Up | #{@base_title}"
   end
 
 end
