193159dc2003fc11e343683e80cc3385f3f5ece9
Joseph Turk <joseph.r.turk@gmail.com>
2015-07-15 20:55:36 -0400

    Finish user signup

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test "invalid user signup" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, user: { name: "",
+                               email: "user@example",
+                               password: "foo",
+                               password_confirmation: "bar" }
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid user signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Test User",
+                                    email: "user@example.com",
+                                    password: "foobar",
+                                    password_confirmation: "foobar" }
+    end
+    assert_template 'users/show'
+  end
+end
